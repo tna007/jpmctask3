d2642bac952869f67911b3dc5b4cbae80a3ad250
tna007 <anh.to@edu.bc.fi>
2022-03-02 19:47:47 +0200

    use perspective to generate a live graph displaying the data feed visually appealing

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,34 @@
-import { ServerRespond } from './DataStreamer';
+import { ServerRespond } from "./DataStreamer";
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  ratio: number;
+  price_abc: number;
+  price_def: number;
+  timestamp: Date;
+  upper_bound: number;
+  lower_bound: number;
+  trigger_alert: number | undefined;
 }
 
-
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]): Row[] {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC = serverResponds[0].top_ask.price;
+    const priceDEF = serverResponds[1].top_ask.price;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.1;
+    const lowerBound = 1 - 0.01;
+    return {
+      ratio: ratio,
+      price_abc: priceABC,
+      price_def: priceDEF,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      timestamp:
+        serverResponds[0].timestamp > serverResponds[1].timestamp
+          ? serverResponds[0].timestamp
+          : serverResponds[1].timestamp,
+      trigger_alert:
+        ratio > upperBound || ratio < lowerBound ? ratio : undefined,
+    };
   }
 }
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,32 +1,37 @@
-import React, { Component } from 'react';
-import { Table } from '@jpmorganchase/perspective';
-import { ServerRespond } from './DataStreamer';
-import { DataManipulator } from './DataManipulator';
-import './Graph.css';
+import React, { Component } from "react";
+import { Table } from "@jpmorganchase/perspective";
+import { ServerRespond } from "./DataStreamer";
+import { DataManipulator } from "./DataManipulator";
+import "./Graph.css";
 
 interface IProps {
-  data: ServerRespond[],
+  data: ServerRespond[];
 }
 
 interface PerspectiveViewerElement extends HTMLElement {
-  load: (table: Table) => void,
+  load: (table: Table) => void;
 }
 class Graph extends Component<IProps, {}> {
   table: Table | undefined;
 
   render() {
-    return React.createElement('perspective-viewer');
+    return React.createElement("perspective-viewer");
   }
 
   componentDidMount() {
     // Get element from the DOM.
-    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = (document.getElementsByTagName(
+      "perspective-viewer"
+    )[0] as unknown) as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+      ratio: "float",
+      price_abc: "float",
+      price_def: "float",
+      timestamp: "date",
+      upper_bound: "float",
+      lower_bound: "float",
+      trigger_alert: "float",
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -35,24 +40,31 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
-      }));
+      elem.setAttribute("view", "y_line"); //'view' is the graph type, previously it was grid type
+      // elem.setAttribute("column-pivots", '["stock"]'); // column-pivots to distinguish stock ABC with DEF in task2, in task3 the objective is to get ratio between 2 stocks so remove it
+      elem.setAttribute("row-pivots", '["timestamp"]'); // "row-pivots" equal to x-axis
+      elem.setAttribute(
+        "columns",
+        '["ratio","upper_bound", "lower_bound", "trigger_alert"]'
+      ); // "columns" to track datapoint's data along y-axis, in task2 only top_ask_price was needed
+      elem.setAttribute(
+        "aggregates",
+        JSON.stringify({
+          ratio: "avg",
+          price_abc: "avg",
+          price_def: "avg",
+          timestamp: "distinct count",
+          upper_bound: "avg",
+          lower_bound: "avg",
+          trigger_alert: "avg",
+        })
+      ); // "aggregates" to handle duplicated data n consolidate it to 1 data point. In this case a data point is considered unique if it has a timestamp. Otherwise, average out all other non-unique fields
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+      this.table.update([DataManipulator.generateRow(this.props.data)]);
     }
   }
 }
